01bba27c712b5fc8c2cb16a32bce8551bc533b6b
NazariySh <shypkanazar22@gmail.com>
2022-05-29 11:25:40 +0300

    up

--- main.js
+++ main.js
@@ -10,10 +10,20 @@ let email = document.getElementById('email');
 let bdate = document.getElementById('bdate');
 let wrap = document.getElementById('wrap');
 let wrap2 = document.querySelector('.body');
-
-
-     
-    
+let registration = document.getElementById('registration');
+let settings = document.getElementById('settings');
+let logout = document.getElementById('logout');
+registration.onclick = ()=>{
+  wrap2.style.display = 'none';
+  wrap.style.display = 'flex';
+  logIn.style.display = 'none';
+  logUp.style.display = 'flex';
+}    
+logout.onclick = ()=>{
+  wrap2.style.display = 'none';
+  wrap.style.display = 'flex';
+ 
+}
 signUpPage.onclick = ()=>{
   logIn.style.display = 'none';
   logUp.style.display = 'flex';
@@ -26,22 +36,8 @@ dropbtn.onclick =()=>{
 
 console.log(localStorage.getItem('users'))
 
-
-function getUsersValues(){
-  const storedValues = localStorage.getItem('users');
-  if(!storedValues){
-    let dataes = {
-      lists:[]
-    };
+function signUPValues(dataes){
  
-    signUp.onclick = ()=>{
-      let API = 'https://randomuser.me/api'
-                    fetch(API)
-                                .then((response) => {
-                                    return response.json();
-                                })
-                                .then((data) =>{
-                                   console.log(data)
       let data1 = Math.floor(Math.random()*100)
       let data2 = Math.floor(Math.random()*(100-data1))
       let data3 = 100 - data1 - data2;
@@ -58,7 +54,7 @@ function getUsersValues(){
       
        
                      let user = {};
-                     user.avatar =  data.results[0].picture.large;
+                  
                      user.name = firsName.value;
                      user.lastName = lastName.value;
                      user.userName = userName.value;
@@ -76,9 +72,15 @@ function getUsersValues(){
                      user.sum4 =  sum4,
                      user.activitySum =  activitySum;
                    
-                 
-                               
+                     fetch('https://randomuser.me/api')
+                                 .then((response) => {
+                                     return response.json();
+                                 })
+                                 .then((data) =>{
+                                    console.log(data)
+                                    user.avatar =  data.results[0].picture.large;
                               
+                    })
                                 
                              
                      
@@ -95,7 +97,7 @@ function getUsersValues(){
                      
                          alert("sign up seccess")
                      
-                         console.log(dataes);
+                        //  console.log(dataes);
                          localStorage.setItem('users', JSON.stringify(dataes))
                      
                          logIn.style.display = 'flex';
@@ -113,8 +115,17 @@ function getUsersValues(){
         
             
        
-                    })
       
+}
+function getUsersValues(){
+  const storedValues = localStorage.getItem('users');
+  if(!storedValues){
+    let dataes = {
+      lists:[]
+    };
+ 
+    signUp.onclick = ()=>{
+      signUPValues(dataes);
       }
       
   
@@ -124,84 +135,7 @@ function getUsersValues(){
   }else{
     let dataes = JSON.parse(storedValues);
     signUp.onclick = ()=>{
-      let API = 'https://randomuser.me/api'
-      fetch(API)
-                  .then((response) => {
-                      return response.json();
-                  })
-                  .then((data) =>{
-                     console.log(data)
-      let data1 = Math.floor(Math.random()*100)
-      let data2 = Math.floor(Math.random()*(100-data1))
-      let data3 = 100 - data1 - data2;
-      let info1 = Math.floor(Math.random()*100)
-      let info2 = Math.floor(Math.random()*100)
-      let info3 = Math.floor(Math.random()*100)
-      let sum1 = Math.floor(Math.random()*10000)
-      let sum2 = Math.floor(Math.random()*10000)
-      let sum3 = Math.floor(Math.random()*10000)
-      let sum4 = Math.floor(Math.random()*10000)
-      let activitySum = Math.floor(Math.random()*50000)
-      
-     
-       
-                     let user = {};
-                     user.name = firsName.value;
-                     user.lastName = lastName.value;
-                     user.userName = userName.value;
-                     user.mail = email.value;
-                     user.birthday = bdate.value;
-                     user.activity1 = data1,
-                     user.activity2 = data2,
-                     user.activity3 =  data3,
-                     user.info1 =  info1,
-                     user.info2 =  info2,
-                     user.info3 =  info3,
-                     user.sum1 =  sum1,
-                     user.sum2 =  sum2,
-                     user.sum3 =  sum3,
-                     user.sum4 =  sum4,
-                     user.activitySum =  activitySum;
-                  
-                    
-                                   user.avatar =  data.results[0].picture.large;
-                                  
-                                
-                     
-                     if(password.value === confirPassword.value){
-                      user.password = password.value;
-                      dataes.lists.push(user);
-                      user.allVisits =  []; 
-                      user.countVisits = 0;
-                      user.statistics = [3,4,1,5,6]
-                     
-                  
-                 
-                  
-                  
-                      alert("sign up seccess")
-                  
-                      console.log(dataes);
-                      localStorage.setItem('users', JSON.stringify(dataes))
-                  
-                      logIn.style.display = 'flex';
-                      logUp.style.display = 'none';
-                  }else{
-                      alert('confirm password false')
-                      password = ' ';
-                      confirPassword = '';
-                  }
-                  
-                             
-      
-                     
-      
-      
-        
-        
-            
-                })
-      
+      signUPValues(dataes);
       
       }
       
@@ -319,7 +253,7 @@ let card = document.getElementById('card')
      
       if(dat.lists[i].userName === logUserName.value && dat.lists[i].password === logPassword.value){
       
-       
+        isGoodPassword = true;
         let name = document.getElementById('name')
         let cardName = document.getElementById('cardName')
         let photo = document.getElementById('photo')
@@ -344,19 +278,21 @@ let card = document.getElementById('card')
               name.innerText = 'Hi ' + dat.lists[i].userName;
            
               photo.style.backgroundImage = 'url('+dat.lists[i].avatar+')';
-              isGoodPassword = true;
+              logUserName.value = '';
+              logPassword.value = '';
+            
         }else{
            isGoodPassword = false;
            
         }
       }
-        if(isGoodPassword == false){
+        if(isGoodPassword == true){
           
           
           logPassword.value = '';
           alert('wrong password')
         }else{
-          console.log('good')
+          console.log('good entrance')
          
         }
   
